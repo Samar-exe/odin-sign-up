35d86b498f5afd8c90508fdf9e5b582cbf1b4093
Samar-exe <samarik2004@gmail.com>
2024-08-02 18:34:54 +0530

    Add constraint validaion api for telephone number

--- script.js
+++ script.js
@@ -10,21 +10,60 @@ password.addEventListener("input", (event) => {
 
 
 // Applying constraint validation api to password input.
+const form = document.querySelector("form");
 const email = document.getElementById("mail")
+const emailError = document.querySelector("#mail + span.email-error");
 email.addEventListener("input", (event) => {
-	if (email.validity.patternMismatch) {
-		email.setCustomValidity("Your email is not in the correct pattern");
+	if (email.validity.valid) {
+		emailError.textContent = ""; // Reset the content of the message
+		emailError.className = "error"; // Reset the visual state of the message
 	} else {
-		email.setCustomValidity("");
+		errorEmail();
 	}
 });
+form.addEventListener("submit", (event) => {
+	// if the email field is valid, we let the form submit
+	if (!email.validity.valid) {
+		// If it isn't, we display an appropriate error message
+		errorEmail();
+		// Then we prevent the form from being sent by canceling the event
+		event.preventDefault();
+	}
+});
+function errorEmail() {
+	if (email.validity.valueMissing) {
+		emailError.textContent = "You need to enter an email address.";
+	} else if (email.validity.typeMismatch) {
+		emailError.textContent = "Entered value needs to be an email address.";
+	} else if (email.validity.tooShort) {
+		emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+	}
+
+	// Set the styling appropriately
+	emailError.className = "error active";
+}
 
 
-const number = document.getElementById("tel")
+
+const number = document.getElementById("tel");
+const telError = document.querySelector("#tel + span.tel-error");
 number.addEventListener("input", (event) => {
-	if (number.validity.patternMismatch) {
-		number.setCustomValidity("Only Indian Numbers are accepted");
-	} else {
-		number.setCustomValidity("");
-	}
+	errorTel();
+	event.preventDefault();
 });
+function errorTel() {
+	if (number.validity.valueMissing) {
+		telError.textContent = "Please enter your phone number"
+	}
+	else if (number.value.length < 10) {
+		telError.textContent = `You need to enter a 10 digit number. Current no. of digits:${number.value.length}`
+	}
+	else {
+		telError.textContent = ""
+	};
+	// if (!number.validity.valid) {
+	// 	console.log("Tel error log working")
+	// }
+	// else {
+
+}
